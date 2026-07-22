cbf8cdbc4a22a9050bc425f337aacecc11164008
Kartik Bansal <kartik.bansal72006@gmail.com>
2026-07-22 10:56:51 +0530

    api_url logged

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -29,6 +29,7 @@ import type {
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
+  console.log("API_BASE_URL =", API_BASE_URL);
 
   async function apiRequest<T>(
   endpoint: string,
