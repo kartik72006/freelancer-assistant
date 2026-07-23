6444eb24d047be80a32ea53c2bd9b35445ec206a
Kartik Bansal <kartik.bansal72006@gmail.com>
2026-07-23 14:24:17 +0530

    Added logs

--- api/dependencies.py
+++ api/dependencies.py
@@ -17,18 +17,22 @@ from services.retrieval.chroma_vector_store import ChromaVectorStore
 from services.retrieval.context_fromatter import ContextFormatter
 from services.retrieval.knowledge_base_indexer import KnowledgeBaseIndexer
 
-
+print("1")
 gemini_service = GeminiService()
-
+print("2")
 embedding_service = EmbeddingService()
+print("3")
 vector_store = ChromaVectorStore()
+print("4")
 semantic_retriever = SemanticRetriever()
+print("5")
 context_formatter = ContextFormatter()
+print("6")
 knowledge_base_indexer = KnowledgeBaseIndexer(
     embedding_service,
     vector_store
 )
-
+print("7")
 retrieval_service = RetrievalService(
     embedding_service=embedding_service,
     vector_store=vector_store,
@@ -36,34 +40,34 @@ retrieval_service = RetrievalService(
     context_formatter=context_formatter,
     knowledge_base_indexer=knowledge_base_indexer,
 )
-
+print("8")
 
 analyzer_agent = AnalyzerAgent()
-
+print("9")
 proposal_agent = ProposalAgent(gemini_service,retrieval_service)
-
+print("10")
 review_agent = ReviewAgent(gemini_service)
-
+print("11")
 orchestrator = AgentOrchestrator(analyzer_agent, proposal_agent, review_agent)
-
+print("12")
 job_repository = JobRepository()
-
+print("13")
 proposal_repository = ProposalRepository()
-
+print("14")
 analytics_repository= AnalyticsRepository()
-
+print("15")
 analytics_service = AnalyticsService(
     analytics_repository=analytics_repository,
     proposal_repository=proposal_repository,
 )
-
+print("16")
 proposal_service = ProposalService(
     orchestrator=orchestrator,
     job_repository=job_repository,
     proposal_repository=proposal_repository,
     analytics_service=analytics_service,
 )
-
+print("17")
 review_service = ReviewService(orchestrator)
-
+print("18")
 analysis_service = AnalysisService(orchestrator)
